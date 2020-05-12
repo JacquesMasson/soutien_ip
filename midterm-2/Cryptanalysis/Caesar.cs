49da3a6a3087d3e357ef19448986acc62354ba25
jacques.masson <jacques.masson@epita.fr>
2020-05-12 11:28:23 +0200

    deuxieme push

--- midterm-2/Cryptanalysis/Caesar.cs
+++ midterm-2/Cryptanalysis/Caesar.cs
@@ -1,27 +1,48 @@
 using System;
+using System.Numerics;
 
 namespace Cryptanalysis
 {
 public class Caesar
 {
+    private int key;
     public Caesar(int key)
     {
-        throw new NotImplementedException();
+        this.key = key;
     }
 
     public string Encrypt(string msg)
     {
-        throw new NotImplementedException();
+        string res = "";
+        for (int i = 0; i < msg.Length; i++)
+        {
+            res += Tools.RotChar(msg[i], key);
+        }
+
+        return res;
     }
 
     public string Decrypt(string cypherText)
     {
-        throw new NotImplementedException();
+        string res = "";
+        for (int i = 0; i < cypherText.Length; i++)
+        {
+            res += Tools.RotChar(cypherText[i], -key);
+        }
+        return res;
     }
     
     public static int GuessKey(string cypherText)
     {
-        throw new NotImplementedException();
+        int[] histo = Tools.Histogram(cypherText);
+        int max = 0;
+        for (int i = 0; i < histo.Length; i++)
+        {
+            if (histo[i] > histo[max]) max=i;
+        }
+
+        int key = max - 4;
+        return key;
     }
 }
 }
